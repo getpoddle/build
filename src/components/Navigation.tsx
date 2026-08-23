c19120886918ce8060cbf42b257d454ab712aa45
getpoddle <getpoddle@gmail.com>
2026-08-23 17:33:20 +0200

    Refactor navigation items for authentication

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -103,12 +103,7 @@ export default function Navigation({ currentPage, onNavigate, collapsed = false,
     }
   }, [onNavigate]);
 
-  const navItemsAuth = [...NAV_ITEMS_AUTH, { id: 'organization', label: 'Organization', icon: Building2 }];
-
-  const navItems = user
-    ? (hideDashboard ? navItemsAuth.filter(i => i.id !== 'home') : navItemsAuth)
-    : NAV_ITEMS_GUEST;
-
+    const navItemsAuth = [...NAV_ITEMS_AUTH, { id: 'organization', label: 'Organization', icon: Building2 }];
   const navItems = user
     ? (hideDashboard ? navItemsAuth.filter(i => i.id !== 'home') : navItemsAuth)
     : NAV_ITEMS_GUEST;
